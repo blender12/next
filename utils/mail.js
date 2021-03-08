f94db796049768b637e14adf66ce996c5d62e7c4
Wasiq <cricketraid200@gmail.com>
2021-03-09 01:54:10 +0530

    first change

--- utils/mail.js
+++ utils/mail.js
@@ -1,13 +1,12 @@
 import nodeMailer from 'nodemailer';
 import {google} from 'googleapis';
-import {oauth2} from 'googleapis/build/src/apis/oauth2';
+/*import {oauth2} from 'googleapis/build/src/apis/oauth2';
 const OAuth2=google.auth.OAuth2;
 
 export const Mail=(email,messg)=>{
 
     const OAuth2client=new OAuth2(
-        '540938705045-6r5usjjv23bgidsnpqlsrq4ecpmevv3e.apps.googleusercontent.com',
-        '22CLPKvns6aUVSRMu6Y0erVc',
+        
         "https://developers.google.com/oauthplayground"
     );
     
@@ -22,9 +21,9 @@ export const Mail=(email,messg)=>{
         auth:{
             type:"OAuth2",
             user:"cricketraid200@gmail.com",
-            clientId:'540938705045-6r5usjjv23bgidsnpqlsrq4ecpmevv3e.apps.googleusercontent.com',
-            clientSecret:'22CLPKvns6aUVSRMu6Y0erVc',
-            refreshToken:'1//04AqJz-mW1ZiICgYIARAAGAQSNwF-L9IrzY480RkyFn5NpEozbS1kk-iwm2GlFNs3qf54lSABe4zRLVmPcLtOIP7Vh40THqHhy64',
+            clientId:
+            clientSecret:
+            refreshToken:'
             accessToken:accessToken
     
         }
@@ -42,5 +41,5 @@ export const Mail=(email,messg)=>{
         console.log("mail sent");
     
     })
-}
+}*/
 
