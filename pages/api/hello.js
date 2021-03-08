23452eed972c76198b80b7f8aefb3afb92d1b99b
Wasiq <cricketraid200@gmail.com>
2021-03-09 00:31:25 +0530

    first change

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,8 +2,7 @@ import {Mail} from '../../utils/mail';
 
 
 export default function handler (req,res){
-    if (req.method === 'POST') {
-         Mail(req.body.Email,req.body.Message);
+        /* Mail(req.body.Email,req.body.Message);*/
          res.json({messg:'sent'});
-      }
+      
 }
