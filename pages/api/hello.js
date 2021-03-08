eed6531aad56c1c5b6c080054710e964bb465dda
Wasiq <cricketraid200@gmail.com>
2021-03-09 01:52:27 +0530

    first change

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,7 +3,7 @@ import {Mail} from '../../utils/mail';
 
 export default async(req,res)=>{
     if (req.method === 'POST') {
-        await res.json({messg:req.body.email});
+        await res.json({messg:req.body.Email});
       }
       res.json("good");
 }
